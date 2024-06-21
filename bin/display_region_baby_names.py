43cca685a7d69570c1c15fad04c7d0f39152cc1c
Pierre Billaud <45881846+PierreBio@users.noreply.github.com>
2024-06-21 12:16:32 +0200

    Initial commit to setup first visualization

---
+++ bin/display_region_baby_names.py
@@ -0,0 +1,29 @@
+import geopandas as gpd
+import plotly.express as px
+import streamlit as st
+
+@st.cache_data
+def load_data():
+    return gpd.read_file('./data/departements-version-simplifiee.geojson')
+
+depts = load_data()
+st.write("Aperçu des données de départements :")
+st.write(depts.head())
+
+depts['id'] = depts['code']
+
+fig = px.choropleth(depts,
+                    geojson=depts.geometry,
+                    locations=depts.index,
+                    color="nom",
+                    hover_name="nom",
+                    hover_data=["code"],
+                    title="Carte Interactive des Départements de France")
+
+fig.update_geos(fitbounds="locations", visible=False)
+
+st.title("Carte Interactive de la France")
+st.write("Carte interactive des départements de France avec Plotly et Streamlit.")
+
+if st.button("Afficher la carte"):
+    st.plotly_chart(fig)
